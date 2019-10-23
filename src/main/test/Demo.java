a09200a35e07eb26897788ddfb6c0cd16fa0acb8
means-88 <1406342572@qq.com>
2019-10-23 15:02:03 +0800

    1.0

---
+++ src/main/test/Demo.java
@@ -0,0 +1,10 @@
+/**
+ * @author Means
+ * @description
+ * @date 2019/10/23
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("----------github" );
+    }
+}
